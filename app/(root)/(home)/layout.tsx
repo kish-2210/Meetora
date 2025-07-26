d248355106baea5a390b17fada269ac9af86b21a
kish-2210 <kishansg32@gmail.com>
2025-07-26 20:32:54 +0530

    File,folder structure & Routing

---
+++ app/(root)/(home)/layout.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from 'react'
+import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
+
+const HomeLayout = ({children}:{children: ReactNode}) => {
+  return (
+    <main>
+        <Navbar/>
+        <div className='flex'>
+          <Sidebar/>
+        </div>
+        <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
+         <div className="w-full">
+        {children}
+
+         </div>
+        </section>
+        Footer
+    </main>
+
+  )
+}
+
+export default HomeLayout
