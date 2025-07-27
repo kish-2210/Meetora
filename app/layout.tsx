630b94b5a3c8aa85e39abf29e10c3d6dcd0c55a3
kish-2210 <kishansg32@gmail.com>
2025-07-27 10:16:24 +0530

    feat: integrate Clerk auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +25,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
+      <ClerkProvider
+      appearance={{
+          layout: {
+            socialButtonsVariant: "iconButton",
+            logoImageUrl: "/icons/logo.svg",
+          },
+          variables: {
+            colorText: "#fff",
+            colorPrimary: "#0E78F9",
+            colorBackground: "#1C1F2E",
+            colorInputBackground: "#252A41",
+            colorInputText: "#fff",
+          },
+        }}
+      >
+
+      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-dark-2`}
       >
         {children}
       </body>
+
+      </ClerkProvider>
     </html>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import MobileNav from './MobileNav'
+import { SignedIn , UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
@@ -18,7 +19,9 @@ const Navbar = () => {
     </Link>
 
     <div className="flex-between gap-5">
-        {/* user management  */}
+      <SignedIn>
+              <UserButton />
+            </SignedIn>
 
         
 
